118e5584b03fa5180c5a3469059f5c6492180b06
Tony <tony.fusco.85@gmail.com>
2022-05-28 23:47:37 +0200

    init

---
+++ lib/antonio_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "antonio_palindrome/version"
+
+module AntonioPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/antonio_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AntonioPalindrome
+  VERSION = "0.1.0"
+end
