ff4a4150fd9d7138d2601b7d115531b6e645d1d3
Tony <tony.fusco.85@gmail.com>
2022-05-29 00:56:45 +0200

    test

---
+++ test/antonio_palindrome_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class AntonioPalindromeTest < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil ::AntonioPalindrome::VERSION
+    end
+    def test_it_does_something_useful
+      assert false
+    end
+end
